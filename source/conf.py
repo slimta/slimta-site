c517e146f2ba018ff670bdeb179fcdcf0bb9f776
Ian Good <ian.good@rackspace.com>
2014-11-19 11:16:46 -0500

    set year to 2014

--- source/conf.py
+++ source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'slimta'
-copyright = u'2013, Ian Good'
+copyright = u'2014, Ian Good'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
