38af7d1bc284032be8551e10a6f06b113a538d35
Ian Good <icgood@gmail.com>
2018-07-10 15:33:21 +0000

    Remove google analytics

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'slimta'
-copyright = u'2016, Ian Good'
+copyright = u'2018, Ian Good'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
